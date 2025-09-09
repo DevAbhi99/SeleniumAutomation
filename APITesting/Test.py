f3d902e24b3342f4994a2c332b18a0d6b080d43e
Abhigyan Khaund <160063176+DevAbhi99@users.noreply.github.com>
2025-09-09 21:39:51 +0100

    Performed performance testing using pytest

--- APITesting/Test.py
+++ APITesting/Test.py
@@ -37,3 +37,58 @@ def testsentData():
     assert body2[1]['id']==2
     assert body2[1]['name']=='Mohan'
     assert body2[1]['age']==24
+
+def testupdateData():
+    payload={"id":2, "name":"Mohan", "age":24}
+    client.post('/sendData', json=payload)
+
+    updatedPayload={"id":2, "name":"Rajesh", "age":24}
+        
+    response=client.put('/updateData/1', json=updatedPayload)
+
+    body1=response.json()
+
+    assert response.status_code==200
+
+    response2=client.get('/getData')
+
+    body2=response2.json()
+
+    assert response2.status_code==200
+
+    assert body1["message"]=="Data updated Successfully"
+
+    assert body2[1]["id"]==2
+    assert body2[1]["name"]=="Rajesh"
+    assert body2[1]["age"]==24
+
+
+def testDeleteData():
+
+     payload={"id":2, "name":"Mohan", "age":24}
+     client.post('/sendData', json=payload)
+
+     response=client.request("DELETE", "/deleteData", json=payload)
+
+     assert response.status_code==200
+
+     body=response.json()
+
+     assert body["message"]=='Data deleted successfully'
+
+    
+def testClearData():
+
+    response=client.delete('/clearData')
+
+    body=response.json()
+
+    assert response.status_code==200
+
+    assert body["message"]=="Data cleared successfully"
+
+
+
+
+
+
